fd5e4367f43aaeffcb60c3e727804dde57851597
Patrick Kamdem <patrick.kamdemkogaing@ionos.com>
2021-05-06 19:43:19 +0200

    some updates

--- src/components/Navbars/KamdemNavbar.js
+++ src/components/Navbars/KamdemNavbar.js
@@ -1,43 +1,55 @@
-
 import React from "react";
-import{ Navbar, Nav, NavDropdown}  from "react-bootstrap";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+// core components
+import KamdemHeader from "components/Navbars/KamdemHeader.js";
+// sections for this page
+import KamdemHeaderLinks from "components/Navbars/KamdemHeaderLinks.js";
+import KamdemGridContainer from "components/Grid/KamdemGridContainer.js";
+import KamdemGridItem from "components/GridItem/KamdemGridItem.js";
+import KamdemParallax from "assets/jss/KamdemParallax.js";
 
-class KamdemNavbar extends React.Component { 
-  render() { 
-    return (
-      <div className="bg-secondary">
-        <div className="container">
-          <Navbar collapseOnSelect expand="lg" variant="dark">
-            <Navbar.Brand href="https://patelot.de">
-              <img className="kamdem-logo" src="img/pk02.svg" alt="Card image cap" style={{ width : '50px'}}/>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link target="_blank" href="https://bitbucket.org/patelotblack/">Projekte</Nav.Link>
-                <Nav.Link target="_blank" href="https://www.lequipe.fr/">Hobbies</Nav.Link>
-                <NavDropdown title="Partners" id="collasible-nav-dropdown">
-                  <NavDropdown.Item target="_blank" href="https://www.kogaing.com">Kogaing Sarl</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item target="_blank" href="https://yanson.de/">Yan'son IT Consulting</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item target="_blank" href="http://www.cclean-services.com">CClean Services</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item target="_blank" href="http://www.pouahom-bw.de">Les Bahamas du BW</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Nav>
-                <Nav.Link href="mailto:patrick-kamdem@patelot.de">Kontakt</Nav.Link>
-                <Nav.Link eventKey={2} target="_blank" href="https://www.ionos.de/">
-                  Mehr
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+
+import styles from "assets/jss/KamdemComponents.js";
+
+const useStyles = makeStyles(styles);
+
+export default function KamdemNavbar(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <KamdemHeader
+        brand="Material Kit React"
+        rightLinks={<KamdemHeaderLinks />}
+        fixed
+        color="transparent"
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <KamdemParallax image={require("assets/img/bg4.jpg")}>
+        <div className={classes.container}>
+          <KamdemGridContainer>
+            <KamdemGridItem>
+              <div className={classes.brand}>
+                <h1 className={classes.title}>Material Kit React.</h1>
+                <h3 className={classes.subtitle}>
+                  A Badass Material-UI Kit based on Material Design.
+                </h3>
+              </div>
+            </KamdemGridItem>
+          </KamdemGridContainer>
         </div>
-      </div>
-    );
-  }
+      </KamdemParallax>
+    </div>
+  );
 }
 
-export default KamdemNavbar;

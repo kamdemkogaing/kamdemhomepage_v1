fd609edb0444f0ecea5b471d781555d04a8431a5
kamdem <patelotblack@bitbucket.org>
2021-05-21 16:17:26 +0200

    new view added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 // core components
 import KamdemNavbar from './components/Navbars/KamdemNavbar.js';
 import KamdemFooter from './components/Footers/KamdemFooter.js';
+import KamdemSectionTabs from './views/Sections/KamdemSectionTabs.js';
 
 function App() {
   return (
     <div className="App">
       <KamdemNavbar />
+      <KamdemSectionTabs />
       <KamdemFooter />
     </div>
   );
---
+++ src/views/Sections/KamdemSectionTabsStyle.js
@@ -0,0 +1,14 @@
+import { container } from "components/jss/KamdemMaterial.js";
+
+const KamdemSectionTabStyle = {
+  section: {
+    background: "#EEEEEE",
+    padding: "70px 0"
+  },
+  container,
+  textCenter: {
+    textAlign: "center"
+  }
+};
+
+export default KamdemSectionTabStyle;

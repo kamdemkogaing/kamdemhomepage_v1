7e3e7fe77b6a168076924498e8f61c89d3aacda5
Patrick Kamdem <patrick.kamdemkogaing@ionos.com>
2021-05-31 13:49:34 +0200

    new section for kamdem app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import KamdemNavbar from './components/Navbars/KamdemNavbar.js';
 import KamdemFooter from './components/Footers/KamdemFooter.js';
 import KamdemSectionTabs from './components/Views/KamdemSectionTabs.js';
 import KamdemSectionLogin from './components/Views/KamdemSectionLogin.js';
+import KamdemWorkSection from "./components/Views/KamdemWorkSection.js";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <KamdemNavbar />
       <KamdemSectionTabs />
       <KamdemSectionLogin />
+      <KamdemWorkSection />
       <KamdemFooter />
     </div>
   );
